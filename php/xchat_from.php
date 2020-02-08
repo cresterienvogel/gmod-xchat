bd87a5b9a37b5b4279c1e24dab87dc53825d38b5
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-02-09 00:12:48 +0700

    a

--- php/xchat_from.php
+++ php/xchat_from.php
@@ -3,7 +3,7 @@
 	$bot_token = 'NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN';
 
 	$ch = curl_init('https://discordapp.com/api/channels/' . $channel_id . '/messages?limit=3');
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bot ' . $bot_token));
+   	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bot ' . $bot_token));
     
 	$output = curl_exec($ch);
 	curl_close($ch);
