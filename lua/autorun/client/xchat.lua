be526b98308fc84b84d6a1ce1e9284f3897e615e
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-06-28 05:58:13 +0700

    Delete xchat.lua

--- lua/autorun/client/xchat.lua
+++
@@ -1,6 +0,0 @@
-net.Receive("xChat", function()
-    local author = net.ReadString()
-    local msg = net.ReadString()
-
-    chat.AddText(Color(127, 159, 255), "[Discord] ", Color(127, 63, 111), author .. ": ", Color(218, 218, 218), msg)
-end)
