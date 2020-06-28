acae87e1ffdc55831d2d715ac03abb0e770569a9
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-06-28 16:44:33 +0700

    Add files via upload

---
+++ php/custom.php
@@ -0,0 +1,29 @@
+<?php
+    $url = $_POST["webhook"];
+    $message = $_POST["message"];
+    $color = $_POST["color"];
+
+    $msg = json_encode([
+        "embeds" => [
+            [
+                "name" => "",
+                "description" => $message,
+                "type" => "rich",
+                "color" => hexdec($color)
+            ]
+        ]
+    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $msg,
+        CURLOPT_HTTPHEADER => [
+            "Content-Type: application/json"
+        ]
+    ]);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+?>
